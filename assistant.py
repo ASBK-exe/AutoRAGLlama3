4c8c027710abaf3b34347c7f50ccf234d4d02dcf
Adam Ben Khalifa <ademsalahbk@gmail.com>
2024-05-12 18:46:40 +0100

    Adding Files

---
+++ assistant.py
@@ -0,0 +1,70 @@
+from typing import Optional
+
+from phi.assistant import Assistant
+from phi.knowledge import AssistantKnowledge
+from phi.llm.ollama import Ollama
+from phi.tools.duckduckgo import DuckDuckGo
+from phi.embedder.ollama import OllamaEmbedder
+from phi.vectordb.pgvector import PgVector2
+from phi.storage.assistant.postgres import PgAssistantStorage
+
+db_url = "postgresql+psycopg://ai:ai@localhost:5532/ai"
+
+
+def get_auto_rag_assistant(
+    llm_model: str = "llama3",
+    embeddings_model: str = "nomic-embed-text",
+    user_id: Optional[str] = None,
+    run_id: Optional[str] = None,
+    debug_mode: bool = True,
+) -> Assistant:
+    """Get an Auto RAG Assistant."""
+
+    # Define the embedder based on the embeddings model
+    embedder = OllamaEmbedder(model=embeddings_model, dimensions=4096)
+    embeddings_model_clean = embeddings_model.replace("-", "_")
+    if embeddings_model == "nomic-embed-text":
+        embedder = OllamaEmbedder(model=embeddings_model, dimensions=768)
+    elif embeddings_model == "phi3":
+        embedder = OllamaEmbedder(model=embeddings_model, dimensions=3072)
+    # Define the knowledge base
+    knowledge = AssistantKnowledge(
+        vector_db=PgVector2(
+            db_url=db_url,
+            collection=f"local_rag_documents_{embeddings_model_clean}",
+            embedder=embedder,
+        ),
+        # 3 references are added to the prompt
+        num_documents=3,
+    )
+
+    return Assistant(
+        name="auto_rag_assistant",
+        run_id=run_id,
+        user_id=user_id,
+        llm=Ollama(model=llm_model),
+        storage=PgAssistantStorage(table_name="auto_rag_assistant", db_url=db_url),
+        knowledge_base=knowledge,
+        description="You are a helpful Assistant called 'AutoRAG' and your goal is to assist the user in the best way possible.",
+        instructions=[
+            "Given a user query, first ALWAYS search your knowledge base using the `search_knowledge_base` tool to see if you have relevant information.",
+            "If you dont find relevant information in your knowledge base, use the `duckduckgo_search` tool to search the internet.",
+            "If you need to reference the chat history, use the `get_chat_history` tool.",
+            "If the users question is unclear, ask clarifying questions to get more information.",
+            "Carefully read the information you have gathered and provide a clear and concise answer to the user.",
+            "Do not use phrases like 'based on my knowledge' or 'depending on the information'.",
+        ],
+        # Show tool calls in the chat
+        show_tool_calls=True,
+        # This setting gives the LLM a tool to search the knowledge base for information
+        search_knowledge=True,
+        # This setting gives the LLM a tool to get chat history
+        read_chat_history=True,
+        tools=[DuckDuckGo()],
+        # This setting tells the LLM to format messages in markdown
+        markdown=True,
+        # Adds chat history to messages
+        add_chat_history_to_messages=True,
+        add_datetime_to_instructions=True,
+        debug_mode=debug_mode,
+    )
